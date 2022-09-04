7a16a8d997654683ebcfb17d88b5b6af4123e9b0
Sergio Damián Bressán <sergiodb95@gmail.com>
2022-09-04 00:17:55 -0300

    first commit

---
+++ Assets/Scripts/GeneticPathfinder.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneticPathfinder : MonoBehaviour
+{
+    public float creatureSpeed;
+    public float pathMultiplier;
+    int pathIndex = 0;
+    public DNA dna;
+    public bool hasFinished = false;
+    public LayerMask obstacleLayer;
+    bool hasBeenInitialized = false;
+    bool hasCrashed = false;
+    bool noHacerMas = false;
+    Vector2 target;
+    Vector2 nextPoint;
+    LineRenderer lr;
+    List<Vector2> travelledPath = new List<Vector2>();
+
+    public void InitCreature(DNA newDna, Vector2 _target)
+    {
+        travelledPath.Add(transform.position);
+        lr = GetComponent<LineRenderer>();
+        dna = newDna;
+        target = _target;
+        nextPoint = transform.position;
+        hasBeenInitialized = true;
+        travelledPath.Add(nextPoint);
+    }
+    private void FixedUpdate()
+    {
+        if (hasBeenInitialized && !hasFinished && !noHacerMas)
+        {
+            if(pathIndex == dna.genes.Count || Vector2.Distance(transform.position, target) < 2f)
+            {
+                hasFinished = true;
+                if(Vector2.Distance(transform.position, target) < 2f){
+                    noHacerMas = true;
+                    Time.timeScale = 0;
+                    return;
+                }
+            }
+            if((Vector2)transform.position == nextPoint)
+            {
+                nextPoint = (Vector2)transform.position + dna.genes[pathIndex] * pathMultiplier;
+                travelledPath.Add(nextPoint);
+                pathIndex++;
+            }
+            else
+            {
+                transform.position = Vector2.MoveTowards(transform.position, nextPoint, creatureSpeed * Time.deltaTime);
+            }
+            RenderLine();
+        }
+    }
+
+    public void RenderLine(){
+        List<Vector3> linePoints = new List<Vector3>();
+        if(travelledPath.Count > 2){
+            for (int i = 0; i < travelledPath.Count - 1; i++)
+            {
+                linePoints.Add(travelledPath[i]);
+            }
+        }
+        else{
+            linePoints.Add(travelledPath[0]);
+        }
+        linePoints.Add(transform.position);
+        lr.positionCount = linePoints.Count;
+        lr.SetPositions(linePoints.ToArray());
+    }
+
+    public float fitness
+    {
+        get
+        {
+            float dist = Vector2.Distance(transform.position, target);
+            if(dist == 0)
+            {
+                dist = 0.0001f;
+            }
+            RaycastHit2D[] obstacles = Physics2D.RaycastAll(transform.position, target, obstacleLayer);
+            float obstacleMultiplier = 1 - (0.15f * obstacles.Length);
+            return (60/dist) * (hasCrashed ? 0.65f : 1f) * obstacleMultiplier;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision){
+        if(collision.gameObject.layer == 8){
+            hasFinished = true;
+            hasCrashed = true;
+        }
+    }
+
+}
---
+++ Assets/Scripts/PopulationController.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PopulationController : MonoBehaviour
+{
+    List<GeneticPathfinder> population = new List<GeneticPathfinder>();
+    public GameObject creaturePrefab;
+    public int populationSize = 100;
+    public int genomeLenght;
+    public float cutoff = 0.3f;
+    public int survivorKeep = 5;
+    [Range(0f,1f)]
+    public float mutationRate = 0.01f;
+    public Transform spawnPoint;
+    public Transform end;
+    public Text text;
+    public int textGen = 0;
+    
+    void InitPopulation()
+    {
+        for(int i = 0; i < populationSize; i++)
+        {
+            GameObject go = Instantiate(creaturePrefab, spawnPoint.position, Quaternion.identity);
+            go.GetComponent<GeneticPathfinder>().InitCreature(new DNA(genomeLenght), end.position);
+            population.Add(go.GetComponent<GeneticPathfinder>());
+        }
+    }
+    void NextGeneration()
+    {
+        textGen++;
+        text.text = textGen.ToString();
+        int survivorCut = Mathf.RoundToInt(populationSize * cutoff);
+        List<GeneticPathfinder> survivors = new List<GeneticPathfinder>();
+        for(int i = 0; i < survivorCut; i++)
+        {
+            survivors.Add(GetFittest());
+        }
+        for(int i = 0; i < population.Count; i++)
+        {
+            Destroy(population[i].gameObject);
+        }
+        population.Clear();
+
+        for (int i = 0; i < survivorKeep; i++)
+        {
+            GameObject go = Instantiate(creaturePrefab, spawnPoint.position, Quaternion.identity);
+            go.GetComponent<GeneticPathfinder>().InitCreature(survivors[i].dna,end.position);
+            population.Add(go.GetComponent<GeneticPathfinder>());
+        }
+        while(population.Count < populationSize)
+        {
+            for(int i = 0; i < survivors.Count; i++)
+            {
+                GameObject go = Instantiate(creaturePrefab, spawnPoint.position, Quaternion.identity);
+                go.GetComponent<GeneticPathfinder>().InitCreature(new DNA(survivors[i].dna, survivors[Random.Range(0, 10)].dna, mutationRate), end.position);
+                population.Add(go.GetComponent<GeneticPathfinder>());
+                if(population.Count >= populationSize)
+                {
+                    break;
+                }
+            }
+        }
+        for(int i = 0; i < survivors.Count; i++)
+        {
+            Destroy(survivors[i].gameObject);
+        }
+    }
+    private void Start()
+    {
+        InitPopulation();
+    }
+    private void FixedUpdate()
+    {
+        if (!HasActive())
+        {
+            NextGeneration();
+        }
+    }
+    GeneticPathfinder GetFittest()
+    {
+        float maxFitness = float.MinValue;
+        int index = 0;
+        for(int i = 0; i < population.Count; i++)
+        {
+            if(population[i].fitness > maxFitness)
+            {
+                maxFitness = population[i].fitness;
+                index = i;
+            }
+        }
+        GeneticPathfinder fittest = population[index];
+        population.Remove(fittest);
+        return fittest;
+    }
+    bool HasActive()
+    {
+        for(int i = 0; i < population.Count; i++)
+        {
+            if (!population[i].hasFinished)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
